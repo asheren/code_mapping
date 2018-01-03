f9c2b08c5d56dc3a54b67f68327f8f9f0e94068d
Allison McMillan <allison.p.mcmillan@gmail.com>
2018-01-03 18:15:53 -0500

    Creates and annotates survey responses table and model

---
+++ app/models/survey_response.rb
@@ -0,0 +1,14 @@
+# == Schema Information
+#
+# Table name: survey_responses
+#
+#  id                         :integer          not null, primary key
+#  perceived_code_confidence  :integer
+#  material_difficulty_rating :integer
+#  notes                      :text
+#  created_at                 :datetime         not null
+#  updated_at                 :datetime         not null
+#
+
+class SurveyResponse < ApplicationRecord
+end
