1c20b4ecbefeb978d4b95c881657c1888d8c4302
Allison McMillan <allison.p.mcmillan@gmail.com>
2018-01-04 21:22:22 -0500

    Raises an error if the client id and client secret are not set.
    This also provides a clear error message for developers who may be deploying
    to make debugging easier.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
+  unless ENV["CLIENT_ID"].present? && ENV["CLIENT_SECRET"].present?
+   raise "client id and client secret are not set. Please set these variables in order to continue"
+  end
   provider :github, ENV["CLIENT_ID"], ENV["CLIENT_SECRET"]
 end
