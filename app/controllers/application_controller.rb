2e0a2c59193dbddab541e0472c4d9229afc91a1e
Allison McMillan <allison.p.mcmillan@gmail.com>
2018-01-08 11:06:56 -0500

    Adds authentication method
    To ensure that a user must be logged in in order to fill out a survey, an
    authentication method was defined.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate
   helper_method :current_user
 
   private
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def authenticate
+    auth = request.env["omniauth.auth"]
+    redirect_to root_path unless auth && User.find_by_provider_and_uid(auth["provider"], auth["uid"])
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  skip_before_action :authenticate, only: [:new]
+
   def new
   end
 end
