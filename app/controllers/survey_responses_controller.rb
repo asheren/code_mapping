b2035e36116298022a2d2d081583ee04e55d7710
Allison McMillan <allison.p.mcmillan@gmail.com>
2018-01-08 22:47:52 -0500

    Added create method

--- app/controllers/survey_responses_controller.rb
+++ app/controllers/survey_responses_controller.rb
@@ -3,4 +3,21 @@ class SurveyResponsesController < ApplicationController
   def new
     @survey_response = SurveyResponse.new
   end
+
+  def create
+    @survey_response = SurveyResponse.new(survey_response_params)
+
+    if @survey_response.save
+      flash[:success] = "Survey was saved"
+      redirect_to survey_responses_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def survey_response_params
+    params.require(:survey_response).permit(:perceived_code_confidence, :material_difficulty_rating, :notes)
+  end
 end
