118a057c0dbedd3479de3b7a19f2d5308431eb90
Allison McMillan <allison.p.mcmillan@gmail.com>
2018-01-25 10:45:02 -0500

    wip commit trying to fix failing spec

--- spec/features/survey_response_new_survey_spec.rb
+++ spec/features/survey_response_new_survey_spec.rb
@@ -57,19 +57,22 @@ feature "Submitting a new code confidence survey", js: true do
     expect(page).to have_content("Index")
   end
 
-  scenario "A user can see all of their survey responses" do
+  scenario "A user can see all of their survey responses", js: true do
     sign_in
     survey_response = create(:survey_response, user: User.last)
     survey_response_2 = create(:survey_response, user: User.last, notes: nil)
-
+    puts SurveyResponse.all.order(created_at: :desc).inspect
+    # Option 1: logging actual SQL query to the DB for ^^ to see if there are differences
+    #Option 2: turn off all db truncation and see if that fixes it
     visit survey_responses_path
-    save_and_open_screenshot
+
+    #print page.html
 
     expect(page.first("tr:nth-of-type(2)")).to have_content(survey_response.created_at.in_time_zone('Eastern Time (US & Canada)').strftime("%b %e, %Y"))
     expect(page.first("tr:nth-of-type(2)")).to have_content(survey_response.perceived_code_confidence)
     expect(page.first("tr:nth-of-type(2)")).to have_content(survey_response.material_difficulty_rating)
     expect(page.first("tr:nth-of-type(2)")).to have_content(survey_response.notes)
-    expect(page.last("tr")).to have_content(survey_response.notes)
+    expect(page.last("tr")).to_not have_content(survey_response.notes)
   end
 
   scenario "A user can delete a survey response" do
